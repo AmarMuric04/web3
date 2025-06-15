7a714750aaaad5ca6c985ff0bd1aeda7f24501d8
Amar Muric Websites <amarmuric2004@gmail.com>
2025-06-16 01:51:22 +0200

    feat: add @types/node dependency and update tsconfig for improved type support

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// main.tsx or index.tsx or wherever your entry point is
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
