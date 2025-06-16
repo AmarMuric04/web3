4f61ff4b95cb199a8c1f69af0397de11f86203a8
Amar Muric Websites <amarmuric2004@gmail.com>
2025-06-16 03:03:27 +0200

    feat: implement Header component for navigation and mobile menu functionality

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
+import Header from "@/components/app-header";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import Header from "../components/header";
 
 export const Route = createRootRoute({
   component: () => (
